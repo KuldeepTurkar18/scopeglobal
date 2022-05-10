c99eb4210f51c6a16390c5a2a04ef0eb99d8fe7d
unknown <priyansh@innoppl.com>
2022-05-10 16:00:44 +0530

    master

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+  $('.owl-carousel').owlCarousel({
+    items: 5,
+    loop:true,
+    autoplay:true,
+    margin:30,
+    dots:false,
+    nav:false,
+    // responsiveClass:true,
+    responsive:{
+        0:{
+            items:2,
+            margin:10,
+        },
+        600:{
+            items:3,
+        },
+        1000:{
+            items:5,
+        }
+    }
+});
+
+$(window).scroll(function(){
+    var sticky = $('header'),
+        scroll = $(window).scrollTop();
+  
+    if (scroll >= 100) sticky.addClass('sticky-header');
+    else sticky.removeClass('sticky-header');
+});
+
